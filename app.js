379e0d23ca26a4e15247d2133c1f9dc3d444966e
Jesus <jesusd.alvarado@gmail.com>
2021-04-11 12:19:12 +0200

    Refactoring and simplifying code

--- app.js
+++ app.js
@@ -1,56 +1,12 @@
-const axios = require('axios');
+import { getFormattedNews } from './utils.js'
 
 
 
 
-function getNewsList(ticker, numNews = 20) {
-  const options = {
-    method: 'GET',
-    url: 'https://seeking-alpha.p.rapidapi.com/news/list',
-    params: {id: ticker, size: `${numNews}`, until: '0'},
-    headers: {
-      'x-rapidapi-key': '30c000177fmshdea0df88a20a59ep1640c4jsna41d72b81051',
-      'x-rapidapi-host': 'seeking-alpha.p.rapidapi.com'
-    }
-  };
+const tickers = ['aapl', 'den']
+const numNews = 5
+const lastDateOfInteres = '2021-04-09'
+const prevDateOfInteres = '2021-02-25'
 
-  axios.request(options).then(function (response) {
-    // console.log(response.data);
-    const data = response.data.data
-    console.log('---General data---')
-    console.log(data)
-    console.log('---News, titles and dates---')
-    data.forEach(noticia => {
-      console.log(noticia.attributes) // con esto podemos ver ya los titulos de las noticias
-    })
-    console.log('---Relationships---')
-    data.forEach(noticia => {
-      console.log(noticia.relationships) // con esto podemos ver ya los titulos de las noticias
-    })
-  }).catch(function (error) {
-    console.error(error);
-  });
-}
-
-function getNewsDetails(id) {
-  const options = {
-    method: 'GET',
-    url: 'https://seeking-alpha.p.rapidapi.com/news/get-details',
-    params: {id: `${id}`},
-    headers: {
-      'x-rapidapi-key': '30c000177fmshdea0df88a20a59ep1640c4jsna41d72b81051',
-      'x-rapidapi-host': 'seeking-alpha.p.rapidapi.com'
-    }
-  };
-
-  axios.request(options).then(function (response) {
-    console.log(response.data);
-  }).catch(function (error) {
-    console.error(error);
-  });
-}
-
-
-
-getNewsList('aapl', 5)
+getFormattedNews(tickers, numNews, lastDateOfInteres, prevDateOfInteres)
 
--- utils.js
+++ utils.js
@@ -1,102 +1,124 @@
-const axios = require('axios');
+import axios from 'axios';
 
-///// news_list
-const options = {
-  method: 'GET',
-  url: 'https://seeking-alpha.p.rapidapi.com/news/list',
-  params: {id: 'aapl', size: '20', until: '0'},
-  headers: {
-    'x-rapidapi-key': '30c000177fmshdea0df88a20a59ep1640c4jsna41d72b81051',
-    'x-rapidapi-host': 'seeking-alpha.p.rapidapi.com'
-  }
-};
+async function getNewsList(ticker='aapl', numNews = 5) {
+  const options = {
+    method: 'GET',
+    url: 'https://seeking-alpha.p.rapidapi.com/news/list',
+    params: {id: ticker, size: `${numNews}`},
+    headers: {
+      'x-rapidapi-key': '30c000177fmshdea0df88a20a59ep1640c4jsna41d72b81051',
+      'x-rapidapi-host': 'seeking-alpha.p.rapidapi.com'
+    }
+  };
 
-axios.request(options).then(function (response) {
-	// console.log(response.data);
+  let titlesDates = []
+  await axios.request(options).then(function (response) {
+    const data = response.data.data
+    data.forEach(news => {
+      titlesDates.push({publishOn: news.attributes['publishOn'], title: news.attributes['title']})
+    })
+  }).catch(function (error) {
+    console.error(error);
+  });
 
-  const data = response.data.data
-  console.log(data, '---Raw data---')
+  return titlesDates
+}
 
-  console.log('---NOTICIAS, TITULOS Y FECHAS DE PUBLICACION---')
-  data.forEach(noticia => {
-    console.log(noticia.attributes) // con esto podemos ver ya los titulos de las noticias
+function getNewsDetails(id) {
+  const options = {
+    method: 'GET',
+    url: 'https://seeking-alpha.p.rapidapi.com/news/get-details',
+    params: {id: `${id}`},
+    headers: {
+      'x-rapidapi-key': '30c000177fmshdea0df88a20a59ep1640c4jsna41d72b81051',
+      'x-rapidapi-host': 'seeking-alpha.p.rapidapi.com'
+    }
+  };
+
+  axios.request(options).then(function (response) {
+    console.log(response.data);
+  }).catch(function (error) {
+    console.error(error);
+  });
+}
+
+function getAllDataForTicker(ticker, numNews = 20) {
+  const options = {
+    method: 'GET',
+    url: 'https://seeking-alpha.p.rapidapi.com/news/list',
+    params: {id: ticker, size: `${numNews}`, until: '0'},
+    headers: {
+      'x-rapidapi-key': '30c000177fmshdea0df88a20a59ep1640c4jsna41d72b81051',
+      'x-rapidapi-host': 'seeking-alpha.p.rapidapi.com'
+    }
+  };
+
+  axios.request(options).then(function (response) {
+    console.log(response.data);
+    const data = response.data.data
+    console.log('---General data---')
+    console.log(data)
+    console.log('---News, titles and dates---')
+    data.forEach(news => {
+      console.log(news.attributes)
+    })
+    console.log('---Relationships---')
+    data.forEach(news => {
+      console.log(news.relationships)
+    })
+  }).catch(function (error) {
+    console.error(error);
+  });
+}
+
+async function getFormattedNews(tickers=['aapl', 'den', 'glt'], numNews=5, lastDateOfInteres='2021-03-12', prevDateOfInteres='2021-03-11') {
+  // const numNews = 5                        // Limit number of news per ticker
+  // const tickers = ['aapl', 'den', 'glt']   // Tickers obtained from Finviz
+  // const lastDateOfInteres = '2021-03-12'
+  // const prevDateOfInteres = '2021-03-11'
+
+  tickers.forEach(async (ticker) => {
+    let titlesDates = await getNewsList(ticker, numNews)
+    console.log('---init', ticker, titlesDates, '---end')
+
+    let filteredDates = filterNewsByDates(titlesDates, lastDateOfInteres, prevDateOfInteres)
+    console.log(`--- ${ticker.toUpperCase()} ---`)
+
+    filteredDates.forEach((el) => {
+      console.log('publishOn:', el['publishOn'])
+      console.log('title:', el['title'], '\n')
+    })
   })
 
-  console.log('---RELATIONSHIPS---')
-  data.forEach(noticia => {
-    console.log(noticia.relationships) // con esto podemos ver ya los titulos de las noticias
+}
+
+function filterNewsByDates(titlesDates, lastDateOfInteres, prevDateOfInteres) {
+
+  let date = null
+  let filteredNewsByDate = []
+
+  titlesDates.forEach((news) => {
+    date = new Date(news['publishOn'])
+
+    let year = date.getFullYear().toString()
+    let month = (date.getMonth()+1).toString()
+    let day = date.getDate().toString()
+    if (month.length===1) {
+      month = '0' + month
+    }
+    if (day.length===1) {
+      day = '0' + day
+    }
+
+    let formattedDate = year + '-' + month + '-' + day
+
+    if (formattedDate===lastDateOfInteres || formattedDate===prevDateOfInteres) {
+      filteredNewsByDate.push(news)
+    }
+
   })
 
+  return filteredNewsByDate
+}
 
-}).catch(function (error) {
-	console.error(error);
-});
-
-
-///// news_get_details
-const options = {
-  method: 'GET',
-  url: 'https://seeking-alpha.p.rapidapi.com/news/get-details',
-  params: {id: '3670877'},
-  headers: {
-    'x-rapidapi-key': '30c000177fmshdea0df88a20a59ep1640c4jsna41d72b81051',
-    'x-rapidapi-host': 'seeking-alpha.p.rapidapi.com'
-  }
-};
-
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
-
-
-///// articles_list
-const options = {
-  method: 'GET',
-  url: 'https://seeking-alpha.p.rapidapi.com/articles/list',
-  params: {category: 'latest-articles', until: '0', size: '20'},
-  headers: {
-    'x-rapidapi-key': '30c000177fmshdea0df88a20a59ep1640c4jsna41d72b81051',
-    'x-rapidapi-host': 'seeking-alpha.p.rapidapi.com'
-  }
-};
-
-axios.request(options).then(function (response) {
-	// console.log(response.data);
-  const data = response.data.data
-  console.log('---Attributes---')
-  data.forEach(article => {
-    console.log(article.attributes)
-  })
-  console.log('---Relationships---')
-  data.forEach(article => {
-    console.log(article.relationships)
-  })
-  console.log('---Links---')
-  data.forEach(article => {
-    console.log(article.links)
-  })
-}).catch(function (error) {
-	console.error(error);
-});
-
-
-// PRIMER EJEMPLO QUE USASTE
-const options = {
-  method: 'GET',
-  url: 'https://seeking-alpha.p.rapidapi.com/auto-complete',
-  params: {term: 'apple'},
-  headers: {
-    'x-rapidapi-key': '30c000177fmshdea0df88a20a59ep1640c4jsna41d72b81051',
-    'x-rapidapi-host': 'seeking-alpha.p.rapidapi.com'
-  }
-};
-
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
-
-
+export {getNewsList, getNewsDetails, getAllDataForTicker, getFormattedNews, filterNewsByDates}
